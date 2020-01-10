3e8e9f949ca651b801b79021d9c3437a7d0f3e00
Sanghee Lee <bigcrow79@gmail.com>
2020-01-10 15:51:55 +0900

    first commit

--- greenplum_init_param.sh
+++ greenplum_init_param.sh
@@ -2,3 +2,8 @@ gpconfig -c gp_workfile_compression -v on --masteronly
 gpconfig -c max_connections -v 1000 -m 500
 gpconfig -c max_prepared_transactions -v 500
 #gpconfig -c gp_vmem_protect_limit -v 16384
+
+
+## Oracle Function
+psql
+ CREATE EXTENSION orafce;
